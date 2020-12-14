8650030e65fc47b1669ccfc6bee119996430d450
Jeremy Langevin <langevinj832@gmail.com>
2020-12-14 11:36:56 -0500

    Added button for generating random mood

--- faceChanger.js
+++ faceChanger.js
@@ -7,14 +7,24 @@ const face = document.getElementById('face');
 //grab all the buttons from the page
 const buttons = Array.from(document.querySelectorAll(".button"));
 
+//grab the random mood selector button
+const randButton = document.querySelector("#rand");
+
 //payloads for each mood
 const moods = {
     "HAPPY": { face: "(＾▽＾)", bg: "yellow" }, 
     "SAD": { face: "●︿●", bg: "blue"},
     "ANGRY": { face: "(⋋▂⋌)", bg: "red" },
-    "CONFUSED": { face: "(◑○◑)", bg: "green"}
+   "CONFUSED": { face: "(◑○◑)", bg: "green"}
 }
 
+randButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    const randNum = Math.floor(Math.random() * Object.keys(moods).length);
+    const mood = Object.keys(moods)[randNum]
+    const payload = moods[mood]
+    store.dispatch({ type: mood, payload: payload });
+});
 
 for (const button of buttons) {
     button.addEventListener("click", function (e) {
