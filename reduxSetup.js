ebf3957711c54541620faede921a1a0d2d76979b
Jeremy Langevin <langevinj832@gmail.com>
2020-12-14 11:28:48 -0500

    cleaned up reducer

--- reduxSetup.js
+++ reduxSetup.js
@@ -4,19 +4,13 @@ function rootReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
         case "HAPPY":
             return { ...state, face: payload.face, bg: payload.bg};
-
         case "SAD":
             return { ...state, face: payload.face, bg: payload.bg };
-
         case "ANGRY":
             return { ...state, face: payload.face, bg: payload.bg };
-        
         case "CONFUSED":
             return { ...state, face: payload.face, bg: payload.bg };
-        
         default:
             return state;
     }
 }
-
-const store = Redux.createStore(rootReducer);
