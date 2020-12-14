9b8b254d24ada1121fb478566dfe714f4e3ca6dd
Jeremy Langevin <langevinj832@gmail.com>
2020-12-14 11:27:52 -0500

    refactored faceChanger to pass payload and added background color

--- faceChanger.js
+++ faceChanger.js
@@ -1,14 +1,33 @@
-window.onload = function() {
-    const faceElement = document.getElementById("face");
+//set store
+const store = Redux.createStore(faceReducer);
 
-    const buttons = Array.from(document.querySelectorAll(".button"))
+//subscribe renderFace to face changes
+const face = document.getElementById('face');
 
-    for(const button of buttons) {
-        button.addEventListener("click", function (e) {
-            e.preventDefault();
-            store.dispatch({ type: e.target.id })
-            const currentFace = store.getState().face;
-            faceElement.innerText = currentFace;
-        })
-    }
+//grab all the buttons from the page
+const buttons = Array.from(document.querySelectorAll(".button"));
+
+//payloads for each mood
+const moods = {
+    "HAPPY": { face: "(＾▽＾)", bg: "yellow" }, 
+    "SAD": { face: "●︿●", bg: "blue"},
+    "ANGRY": { face: "(⋋▂⋌)", bg: "scarlet" },
+    "CONFUSED": { face: "(◑○◑)", bg: "green"}
 }
+
+
+for (const button of buttons) {
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+        const payload = moods[e.target.id]
+        store.dispatch({ type: e.target.id, payload: payload })
+    });
+}
+
+function renderFace() {
+    face.innerText = store.getState().face;
+    face.style.cssText = `background-color: ${store.getState().bg}`;
+}
+
+renderFace()
+store.subscribe(renderFace)
--- reduxSetup.js
+++ reduxSetup.js
@@ -1,18 +1,18 @@
-const INITIAL_STATE = { face: "┐(´～｀) ┌" }
+const INITIAL_STATE = { face: "┐(´～｀) ┌", bg: "white" }
 
 function rootReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
         case "HAPPY":
-            return { ...state, face: "(＾▽＾)"};
+            return { ...state, face: payload.face, bg: payload.bg};
 
         case "SAD":
-            return { ...state, face: "●︿●" }
+            return { ...state, face: payload.face, bg: payload.bg };
 
         case "ANGRY":
-            return { ...state, face: "(⋋▂⋌)" }
+            return { ...state, face: payload.face, bg: payload.bg };
         
         case "CONFUSED":
-            return { ...state, face: "(◑○◑)" }
+            return { ...state, face: payload.face, bg: payload.bg };
         
         default:
             return state;
